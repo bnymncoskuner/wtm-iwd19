70f135d91867f730c10a7f833524c730ed749ba3
Bunyamin Coskuner <bnymncoskuner@gmail.com>
2019-03-17 23:05:31 +0300

    Improve users-panel

--- wtm-chat/src/app/users-panel/users-panel.component.ts
+++ wtm-chat/src/app/users-panel/users-panel.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from '../core/services/user.service';
+
 @Component({
   selector: 'wtm-users-panel',
   templateUrl: './users-panel.component.html',
-  styleUrls: ['./users-panel.component.sass']
+  styleUrls: ['./users-panel.component.scss']
 })
 export class UsersPanelComponent implements OnInit {
 
-  constructor() { }
+  users$;
+
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.users$ = this.userService.getUsers();
   }
 
 }
--- wtm-chat/src/app/users-panel/users-panel.module.ts
+++ wtm-chat/src/app/users-panel/users-panel.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersPanelComponent } from './users-panel.component';
+import { UserCountPipe } from './user-count.pipe';
 
 @NgModule({
-  declarations: [UsersPanelComponent],
+  declarations: [UsersPanelComponent, UserCountPipe],
   imports: [
     CommonModule
   ],
