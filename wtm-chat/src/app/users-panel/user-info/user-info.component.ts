41f1f8ac2af866a3d4d79bc5cfc43c1895b8f182
Bunyamin Coskuner <bnymncoskuner@gmail.com>
2019-03-17 23:15:37 +0300

    Generate user info

---
+++ wtm-chat/src/app/users-panel/user-info/user-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wtm-user-info',
+  templateUrl: './user-info.component.html',
+  styleUrls: ['./user-info.component.sass']
+})
+export class UserInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- wtm-chat/src/app/users-panel/users-panel.module.ts
+++ wtm-chat/src/app/users-panel/users-panel.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersPanelComponent } from './users-panel.component';
 import { UserCountPipe } from './user-count.pipe';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 @NgModule({
-  declarations: [UsersPanelComponent, UserCountPipe],
+  declarations: [UsersPanelComponent, UserCountPipe, UserInfoComponent],
   imports: [
     CommonModule
   ],
