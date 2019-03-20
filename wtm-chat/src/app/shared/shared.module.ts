e95fe1a3a7554a4d2f1f071618b7ef68e0e65c01
Bunyamin Coskuner <bnymncoskuner@gmail.com>
2019-03-21 00:16:04 +0300

    Import material modules

--- wtm-chat/src/app/shared/shared.module.ts
+++ wtm-chat/src/app/shared/shared.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatInputModule, MatIconModule, MatFormFieldModule, MatButtonModule } from '@angular/material';
 import { InputComponent } from './components/input/input.component';
 
 @NgModule({
   declarations: [InputComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   exports: [InputComponent]
 })
