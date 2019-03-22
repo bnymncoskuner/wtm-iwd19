90817f0782ae3372ef6eaf50bdfed67ba8ef6d14
Bunyamin Coskuner <bnymncoskuner@gmail.com>
2019-03-22 23:53:59 +0300

    Add mat drawer

--- wtm-chat/src/app/app.module.ts
+++ wtm-chat/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { CoreModule } from './core/core.module';
 import { UsersPanelModule } from './users-panel/users-panel.module';
 import { MessagePanelModule } from './message-panel/message-panel.module';
 
 import { AppComponent } from './app.component';
+import { MatButtonModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     CoreModule,
     UsersPanelModule,
-    MessagePanelModule
+    MessagePanelModule,
+    MatSidenavModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
