244a893cf8d28caeece1604de7e9811bf988c4cc
Bunyamin Coskuner <bnymncoskuner@gmail.com>
2019-03-23 07:45:01 +0300

    Change layout

--- wtm-chat/src/app/app.module.ts
+++ wtm-chat/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material';
 import { CoreModule } from './core/core.module';
 import { UsersPanelModule } from './users-panel/users-panel.module';
 import { MessagePanelModule } from './message-panel/message-panel.module';
@@ -16,7 +18,9 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     CoreModule,
     UsersPanelModule,
-    MessagePanelModule
+    MessagePanelModule,
+    MatSidenavModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
