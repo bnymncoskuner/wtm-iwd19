472f6ba9ed497b4b2b61dfab645d1422bd22aba5
Bunyamin Coskuner <bnymncoskuner@gmail.com>
2019-03-23 07:41:47 +0300

    Fix imports

--- wtm-chat/src/app/app.module.ts
+++ wtm-chat/src/app/app.module.ts
@@ -2,12 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material';
 import { CoreModule } from './core/core.module';
 import { UsersPanelModule } from './users-panel/users-panel.module';
 import { MessagePanelModule } from './message-panel/message-panel.module';
 
 import { AppComponent } from './app.component';
-import { MatButtonModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
